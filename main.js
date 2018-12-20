454fff55057bbf4a63cbed674fa97144c0fe2f25
Saif Shovon <saaifshovon@gmail.com>
2018-12-20 22:21:27 +0600

    Part 10: Practice Component Bulma Modal

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 
-Vue.component('message',{
-	props : ['title','description'],
-	data(){
-		return{
-			isVissible : true
-		}
-	},
-	template : '<article v-show = "isVissible" class="message"><div class="message-header"><p>{{title}}</p><button @click = "isVissible = false" class="delete" aria-label="delete"></button></div><div class="message-body">{{description}}</div></article>'
+Vue.component('modal',{
+template : `
+<div class="modal is-active">
+
+	<div class="modal-background"></div>
+	<div class="modal-content">
+
+		<div class = "box"><slot></slot></div>
+
+	</div>
+	<button  class="modal-close is-large"  @click = "$emit('close')"></button>
+
+</div>`
 });
 
 new Vue({
-	el: '#root'
+	el: '#root',
+	data : {
+		showModal : false
+	}
 });
